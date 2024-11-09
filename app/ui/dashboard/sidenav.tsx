bb31589e0233382f1228b5af9c31e96fe98c3232
Marvin Xu <marvinxu99@hotmail.com>
2024-11-09 13:30:56 -0800

    ...

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -19,6 +19,9 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="w-full rounded-md bg-gray-50 p-2 text-sm text-gray-700 md:block">
+          logged in user
+        </div>
         <form 
           action={async () => {
             'use server';
